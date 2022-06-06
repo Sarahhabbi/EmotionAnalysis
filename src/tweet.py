214b93a27e78d35b0ecd96c53c783fee4b41751b
Sarahhabbi <habbisarah.1@gmail.com>
2022-06-06 16:17:48 +0200

    remove tweepy

--- src/tweet.py
+++ src/tweet.py
@@ -1,5 +1,5 @@
 import snscrape.modules.twitter as sntwitter
-import tweepy
+
 
 def get_tweets(hashtag, limit):
     # Creating list to append tweet data to
@@ -12,18 +12,3 @@ def get_tweets(hashtag, limit):
         tweets_list.append(tweet.content)
         
     return tweets_list
-
-
-# consumer_key = "GtINquEQo66y1LcKPCEya2g0F"
-# consumer_secret = "ObSLCR6leequ1DVNAOBps4W86jYQfNxU06JVgU9epFqrPqgkEO"
-# access_token = "1529886446546468865-XL67Mgnz6dsV0EyAyIEqgNR7rkOqgw"
-# access_token_secret = "k4Hlxhm5PDHR4u29vYfP9NjYBWaGN3l9GCdQdNyK1VdvJ"
-# bearer = "AAAAAAAAAAAAAAAAAAAAANoXdAEAAAAA7rjk0WH%2FcDax3B6ni9AB%2FTaRSys%3DS6KbCxTizG32aHmRJqWc0zqyvCoyM22sabqPN7mhrncmhBOldh"
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_token_secret)
-
-# api = tweepy.API(auth) 
-
-# res = api.search('lang','en')
-# for i in res:
-#     print(i.lang)
